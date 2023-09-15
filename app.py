483b7e906b15363d84828663ee8d1db2bdc13ddb
dzelle-tan <dzellefaith@gmail.com>
2023-09-15 22:37:49 +0800

    [DZ] add input validations, error handling, fix account suspension, etc.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request,render_template, redirect,session
+from flask import Flask, request,render_template, redirect,session, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 import bcrypt
 import time
 
@@ -44,54 +45,56 @@ def register():
         email = request.form['email']
         password = request.form['password']
 
-        new_user = User(name=name,email=email,password=password)
+        # Check if any of the fields are empty
+        if not name or not email or not password:
+            return render_template('register.html', error='Please fill in all fields.')
+
+        # Check if the email already exists in the database
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            return render_template('register.html', error='Email address already exists. Please use a different email.')
+        
+        if len(password) < 8:
+            return render_template('register.html', error='Password must be at least 8 characters.')
+        
+        new_user = User(name=name, email=email, password=password)
         db.session.add(new_user)
-        db.session.commit()
-        return redirect('/login')
-
-
+        try:
+            db.session.commit()
+            flash('Account created successfully. You can now log in.', 'success')
+            return redirect('/login')
+        except IntegrityError:
+            db.session.rollback()
+            return render_template('register.html', error='An error occurred while creating your account. Please try again.')
 
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    initialize_login_attempts()  # Initialize login attempts session variable
-    suspended = False
-    remaining_time = 0
-
-    if session['login_attempts'] >= 3:
-        suspension_duration = 60  # Duration in seconds
-        suspended = True
-        if time.time() - session['last_failed_login'] > suspension_duration:
-            suspended = False
-        else:
-            # If suspended, calculate remaining time
-            remaining_time = int(suspension_duration - (time.time() - session['last_failed_login']))
-            if remaining_time < 0:
-                remaining_time = 0
-
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
 
         user = User.query.filter_by(email=email).first()
 
-        if user and user.check_password(password) and not suspended:
+        if user and user.check_password(password):
             session['email'] = user.email
             session['login_attempts'] = 0  # Reset login attempts on successful login
             return redirect('/dashboard')
+        elif user is None:
+            return render_template('login.html', error='User not found. Please <a href="/register">register</a>.')
         else:
             session['login_attempts'] += 1  # Increment login attempts
 
-            if session['login_attempts'] >= 3:
-                session['last_failed_login'] = time.time()
+            if session['login_attempts'] == 3:
+                return render_template('suspended.html')
+            
+            if session['login_attempts'] >= 6:
+                session['login_attempts'] = 0
+                return render_template('locked.html')
 
-            if suspended:
-                return render_template('login.html', error=f'Invalid username or password. Login again after {remaining_time} seconds.', suspended=True)
-            else:
-                return render_template('login.html', error='Invalid username or password.', suspended=suspended)
-
-    return render_template('login.html', suspended=suspended, remaining_time=remaining_time)
+    return render_template('login.html', error='Invalid Username or Password.')
 
 @app.route('/dashboard')
 def dashboard():
